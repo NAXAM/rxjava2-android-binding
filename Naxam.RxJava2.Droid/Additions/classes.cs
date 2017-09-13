dda4746f38561ce0b91fcbac79a4f92d99e74a93
morejump <thao@naxam.net>
2017-09-13 15:54:53 +0700

    updating metadeta and classes

--- Naxam.RxJava2.Droid/Additions/classes.cs
+++ Naxam.RxJava2.Droid/Additions/classes.cs
@@ -96,6 +96,29 @@ namespace IO.Reactivex.Internal.Functions
 {
     partial class Functions
     {
+        partial class ToMultimapKeyValueSelector
+        {
+            public unsafe void Accept(Java.Lang.Object p0, Java.Lang.Object p1)
+            {
+                Accept((Java.Lang.Object)p0, (Java.Lang.Object)p1);
+            }
+
+        }
+        partial class ToMapKeyValueSelector
+        {
+            public unsafe void Accept(Java.Lang.Object p0, Java.Lang.Object p1)
+            {
+                Accept((Java.Lang.Object)p0, (Java.Lang.Object)p1);
+            }
+
+        }
+        partial class ToMapKeySelector
+        {
+            public unsafe void Accept(Java.Lang.Object p0, Java.Lang.Object p1)
+            {
+                Accept((Java.Lang.Object)p0, (Java.Lang.Object)p1);
+            }
+        }
         partial class Array2Func
         {
             public unsafe Java.Lang.Object Apply(Java.Lang.Object a)
@@ -168,140 +191,99 @@ namespace IO.Reactivex.Internal.Functions
                 return (Java.Lang.Object)CallForData();
             }
         }
-    }
-}
 
-namespace IO.Reactivex.Schedulers
-{
-    partial class Schedulers
+        partial class TimestampFunction
+        {
+
+            public Java.Lang.Object Apply(Java.Lang.Object Object)
+            {
+                return (Java.Lang.Object)ApplyMapper(Object);
+
+            }
+        }
+    }
+
+    namespace IO.Reactivex.Schedulers
     {
-        partial class ComputationTask
+        partial class Schedulers
         {
-            public Java.Lang.Object Call()
+            partial class ComputationTask
             {
-                return (Java.Lang.Object)CallForData();
+                public Java.Lang.Object Call()
+                {
+                    return (Java.Lang.Object)CallForData();
+                }
+            }
+            partial class IOTask
+            {
+                public Java.Lang.Object Call()
+                {
+                    return (Java.Lang.Object)CallForData();
+                }
+            }
+            partial class NewThreadTask
+            {
+                public Java.Lang.Object Call()
+                {
+                    return (Java.Lang.Object)CallForData();
+                }
+            }
+            partial class SingleTask
+            {
+                public Java.Lang.Object Call()
+                {
+                    return (Java.Lang.Object)CallForData();
+                }
             }
         }
-        partial class IOTask
-        {
-            public Java.Lang.Object Call()
-            {
-                return (Java.Lang.Object)CallForData();
-            }
-        }
-        partial class NewThreadTask
-        {
-            public Java.Lang.Object Call()
-            {
-                return (Java.Lang.Object)CallForData();
-            }
-        }
-        partial class SingleTask
-        {
-            public Java.Lang.Object Call()
-            {
-                return (Java.Lang.Object)CallForData();
-            }
-        }
+
     }
 
-}
-
-namespace IO.Reactivex.Internal.Operators.Observable
-{
-    partial class ObservableInternalHelper
+    namespace IO.Reactivex.Internal.Operators.Observable
     {
-        partial class BufferedReplayCallable
+        partial class ObservableInternalHelper
         {
-            public Java.Lang.Object Call()
+            partial class BufferedReplayCallable
             {
-                return (Java.Lang.Object)CallForData();
+                public Java.Lang.Object Call()
+                {
+                    return (Java.Lang.Object)CallForData();
+                }
             }
-        }
-        partial class BufferedTimedReplayCallable
-        {
-            public Java.Lang.Object Call()
+            partial class BufferedTimedReplayCallable
             {
-                return (Java.Lang.Object)CallForData();
+                public Java.Lang.Object Call()
+                {
+                    return (Java.Lang.Object)CallForData();
+                }
             }
-        }
-        partial class ReplayCallable
-        {
-            public Java.Lang.Object Call()
+            partial class ReplayCallable
             {
-                return (Java.Lang.Object)CallForData();
+                public Java.Lang.Object Call()
+                {
+                    return (Java.Lang.Object)CallForData();
+                }
             }
-        }
-        partial class TimedReplayCallable
-        {
-            public Java.Lang.Object Call()
+            partial class TimedReplayCallable
             {
-                return (Java.Lang.Object)CallForData();
+                public Java.Lang.Object Call()
+                {
+                    return (Java.Lang.Object)CallForData();
+                }
             }
         }
     }
-}
 
-namespace IO.Reactivex.Internal.Util
-{
-    partial class ArrayListSupplier
+    namespace IO.Reactivex.Internal.Util
     {
-        public Java.Lang.Object Call()
-        {
-            return (Java.Lang.Object)CallForData();
-        }
-    }
-    partial class HashMapSupplier
-    {
-        public Java.Lang.Object Call()
-        {
-            return (Java.Lang.Object)CallForData();
-        }
-    }
-}
-
-namespace IO.Reactivex.Internal.Operators.Flowable
-{
-    partial class FlowableInternalHelper
-    {
-        partial class BufferedReplayCallable
+        partial class ArrayListSupplier
         {
             public Java.Lang.Object Call()
             {
                 return (Java.Lang.Object)CallForData();
             }
         }
-        partial class BufferedTimedReplayCallable
-        {
-            public Java.Lang.Object Call()
-            {
-                return (Java.Lang.Object)CallForData();
-            }
-        }
-        partial class ReplayCallable
-        {
-            public Java.Lang.Object Call()
-            {
-                return (Java.Lang.Object)CallForData();
-            }
-        }
-        partial class TimedCallable
-        {
-            public Java.Lang.Object Call()
-            {
-                return (Java.Lang.Object)CallForData();
-            }
-        }
-
-        partial class BufferedTimedReplay
-        {
-            public Java.Lang.Object Call()
-            {
-                return (Java.Lang.Object)CallForData();
-            }
-        }
-
-        partial class TimedReplay
+        partial class HashMapSupplier
         {
             public Java.Lang.Object Call()
             {
@@ -310,30 +292,123 @@ namespace IO.Reactivex.Internal.Operators.Flowable
         }
     }
 
-    partial class FlowableReplay
+    namespace IO.Reactivex.Internal.Operators.Flowable
     {
-        partial class ReplayBufferTask
+        partial class FlowableInternalHelper
         {
-            public Java.Lang.Object Call()
+            partial class BufferedReplayCallable
             {
-                return (Java.Lang.Object)CallForData();
+                public Java.Lang.Object Call()
+                {
+                    return (Java.Lang.Object)CallForData();
+                }
             }
-        }
-        partial class ScheduledReplayBufferTask
-        {
-            public Java.Lang.Object Call()
+            partial class BufferedTimedReplayCallable
             {
-                return (Java.Lang.Object)CallForData();
+                public Java.Lang.Object Call()
+                {
+                    return (Java.Lang.Object)CallForData();
+                }
             }
-        }
-    }
-}
+            partial class ReplayCallable
+            {
+                public Java.Lang.Object Call()
+                {
+                    return (Java.Lang.Object)CallForData();
+                }
+            }
+            partial class TimedCallable
+            {
+                public Java.Lang.Object Call()
+                {
+                    return (Java.Lang.Object)CallForData();
+                }
+            }
 
-namespace IO.Reactivex.Internal.Operators.Single
-{
-    partial class SingleInternalHelper
+            partial class BufferedTimedReplay
+            {
+                public Java.Lang.Object Call()
+                {
+                    return (Java.Lang.Object)CallForData();
+                }
+            }
+
+            partial class TimedReplay
+            {
+                public Java.Lang.Object Call()
+                {
+                    return (Java.Lang.Object)CallForData();
+                }
+            }
+            //
+            partial class ItemDelayFunction
+            {
+                public Java.Lang.Object Apply(Java.Lang.Object Object)
+                {
+                    return (Java.Lang.Object)ApplyMapper(Object);
+
+                }
+
+
+            }
+
+            partial class FlatMapWithCombinerOuter
+            {
+                public Java.Lang.Object Apply(Java.Lang.Object Object)
+                {
+                    return (Java.Lang.Object)ApplyMapper(Object);
+
+                }
+
+
+            }
+            partial class FlatMapIntoIterable
+            {
+                public Java.Lang.Object Apply(Java.Lang.Object Object)
+                {
+                    return (Java.Lang.Object)ApplyMapper(Object);
+
+                }
+
+
+            }
+        }
+
+        partial class FlowableReplay
+        {
+            partial class ReplayBufferTask
+            {
+                public Java.Lang.Object Call()
+                {
+                    return (Java.Lang.Object)CallForData();
+                }
+            }
+            partial class ScheduledReplayBufferTask
+            {
+                public Java.Lang.Object Call()
+                {
+                    return (Java.Lang.Object)CallForData();
+                }
+            }
+        }
+    }
+
+    namespace IO.Reactivex.Internal.Operators.Single
     {
-        partial class NoSuchElementCallable
+        partial class SingleInternalHelper
+        {
+            partial class NoSuchElementCallable
+            {
+                public Java.Lang.Object Call()
+                {
+                    return (Java.Lang.Object)CallForData();
+                }
+            }
+        }
+    }
+    namespace IO.Reactivex.Internal.Schedulers
+    {
+        partial class ScheduledDirectTask
         {
             public Java.Lang.Object Call()
             {
@@ -341,14 +416,72 @@ namespace IO.Reactivex.Internal.Operators.Single
             }
         }
     }
-}
-namespace IO.Reactivex.Internal.Schedulers
-{
-    partial class ScheduledDirectTask
+    namespace IO.Reactivex.Internal.Operators.Single
     {
-        public Java.Lang.Object Call()
+        partial class SingleInternalHelper
         {
-            return (Java.Lang.Object)CallForData();
+            partial class ToFlowableIterator
+            {
+                public Java.Lang.Object Next()
+                {
+                    return (Java.Lang.Object)NextItem();
+                }
+
+            }
+
+
         }
+
     }
+    namespace IO.Reactivex.Internal.Operators.Observable
+    {
+
+        partial class ObservableInternalHelper
+        {
+            partial class ObservableMapper
+            {
+                public Java.Lang.Object Apply(Java.Lang.Object Object)
+                {
+                    return (Java.Lang.Object)ApplyMapper(Object);
+
+                }
+
+            }
+
+            partial class ItemDelayFunction
+            {
+                public Java.Lang.Object Apply(Java.Lang.Object Object)
+                {
+                    return (Java.Lang.Object)ApplyMapper(Object);
+
+                }
+
+
+            }
+
+            partial class FlatMapWithCombinerOuter
+            {
+
+                public Java.Lang.Object Apply(Java.Lang.Object Object)
+                {
+                    return (Java.Lang.Object)ApplyMapper(Object);
+
+                }
+            }
+
+            partial class FlatMapIntoIterable
+            {
+
+                public Java.Lang.Object Apply(Java.Lang.Object Object)
+                {
+                    return (Java.Lang.Object)ApplyMapper(Object);
+
+                }
+            }
+
+        }
+
+    }
+
+
 }
